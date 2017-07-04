e819f430a43ff3135472284d158bd35c82a520ac
xienb <xienb@qq.com>
2017-07-04 11:51:28 +0800

    11

---
+++ DotNetTurorial.Common/SQLHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace DotNetTurorial.Common
+{
+    public class SQLHelper
+    {
+        public const string _conStr="server=127.0.0.1;port=3306;user id=root;password=123456;database=dotnetcore;pooling=false";
+        public static int AddStudent(string name,int gender,string phone)
+        {
+            MySqlConnection connect=new MySqlConnection(_conStr);
+            var ret = connect.Execute("insert into student(name,gender,phone) values(@name,@gender,@phone)",new {name=name,gender=gender,phone=phone});
+            return ret;
+        }
+    }
+}
---
+++ DotNetTurorial.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DotNetTurorial.Common;
+
+namespace DotNetTurorial.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            //Console.OutputEncoding = Encoding.UTF8; // 设置控制台编码
+            AddUser();
+            while(Console.ReadLine()!="exit"){
+                AddUser();
+            }
+        } 
+        static void AddUser()
+        {
+            Console.WriteLine("please enter name:");
+            var name=Console.ReadLine();
+            Console.WriteLine("please enter gender,1 for male and 0 for female.");
+            var gender = Console.ReadLine();
+            Console.WriteLine("please enter phone:");
+            var phone = Console.ReadLine();
+
+            var ret = SQLHelper.AddStudent(name,Convert.ToInt32(gender),phone);
+            if(ret>0)
+            {
+                 Console.WriteLine("success");
+            }
+        }
+    }
+}
